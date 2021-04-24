d5e6025d535523fab4de6d6b4544b27651c534d0
Henrique <henrique.f.ibiapino@gmail.com>
2021-04-24 14:49:24 -0300

    Create usuarios

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
+
+//indica para o express que usaremos JSON
+app.use(express.json());
 
 let usuarios = [
     {nome: "Henrique", sobrenome: "Ibiapino"}
@@ -16,8 +20,23 @@ app.get("/usuarios", (request, response) => {
     return response.json(usuarios);
 })
 
-app.listen(3000, () => {
+app.post("/usuarios", (request, response) => {
+    const { nome, sobrenome }  = request.body;
+
+     usuarios.push({ nome, sobrenome});
+     
+     return response.json({nome, sobrenome});
+})
+
+//http://localhost:3000/usuarios/henrique
+app.put("/usuarios/:sobrenome", (request, response) => {
+  const { nome } = request.params;
+
+
+  const { sobrenome } = reques.body;
+})
+
+app.listen(3000, (req, res) => {
     
     console.log('servidor rodando!');
 })
-
